a9591111f030ca58b1c18d44484b5df6bbdf0b13
nasirdiu <nasir.joypurhat@gmail.com>
2022-10-02 22:38:39 +0600

    count

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Home from './Home.vue';
 </script>
 
 <template>
@@ -15,6 +16,7 @@ import TheWelcome from './components/TheWelcome.vue'
   <main>
     <TheWelcome />
   </main>
+  <Home/>
 </template>
 
 <style scoped>
---
+++ src/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button v-on:mouseover="hello('data')">Count</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        methods:{
+            hello(item){
+                console.log(item);
+            }
+        }
+    }
+</script>
