222714556cb8013932e4aae4f932900f9b4d01ad
nasir <nasir.joypurhat@gmail.com>
2023-12-11 13:00:54 +0600

    if -show

--- condition/src/App.vue
+++ condition/src/App.vue
@@ -4,12 +4,14 @@ const weather = 's'
 </script>
 
 <template>
+  <!-- <template v-if="weather"></template> div er jaigai remplate use kora jai but at maje v-show use kora jai na..... v-if new vabe ase v-show thake jai new vabe korer dorkar pore na -->
+
   <h1 class="text-red-600 text-3xl">Logic if else in vue js</h1>
-  <h1 class="text-green-600 text-2xl" v-if="weather=='s'">Weather :Sunny</h1>
-  <h1 class="text-green-600 text-2xl"  v-show="weather=='pc'">Weather :Partly Cloudy</h1>
-  <h1 class="text-green-600 text-2xl" v-show="weather=='r'">Weather :Rainy</h1>
-  <h1 class="text-green-600 text-2xl"  v-show="weather=='t'">Weather :Thunderstore</h1>
-  <h1 class="text-green-600 text-2xl" v-show="weather==c">Weather :Cyclone</h1>
+  <h1 class="text-green-600 text-2xl" v-if="weather == 's'">Weather :Sunny</h1>
+  <h1 class="text-green-600 text-2xl" v-show="weather == 'pc'">Weather :Partly Cloudy</h1>
+  <h1 class="text-green-600 text-2xl" v-show="weather == 'r'">Weather :Rainy</h1>
+  <h1 class="text-green-600 text-2xl" v-show="weather == 't'">Weather :Thunderstore</h1>
+  <h1 class="text-green-600 text-2xl" v-show="weather == c">Weather :Cyclone</h1>
 </template>
 
 <style scoped></style>
