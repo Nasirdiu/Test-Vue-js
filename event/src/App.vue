7c8c9c959e60e4faa96c1e3f13a88c4925d6beb8
nasir <nasir.joypurhat@gmail.com>
2023-12-12 10:34:52 +0600

    event current location

--- event/src/App.vue
+++ event/src/App.vue
@@ -1,12 +1,23 @@
 <script setup>
 
+import { ref } from 'vue';
+const location =ref( "Dhaka")
+
+function changeLocation(newLocation) {
+  location.value=newLocation;
+}
 </script>
 
 <template>
- 
-
+  <section class="container mx-auto flex items-center flex-col">
+    <h1 class="text-center text-2xl py-10">Event & Reactivity in vue.js</h1>
+    <div class="container mx-auto flex space-x-5 justify-center m-5">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Dhaka')">Dhaka</button>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Bogra')">Bogra</button>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="changeLocation('Panthopoth')">Panthopoth</button>
+    </div>
+    <h1 class="text-2xl m-10">Current Location is {{ location }}</h1>
+  </section>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
