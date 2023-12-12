db1ccde10e68425863dfbb396ec441edc25786d3
nasir <nasir.joypurhat@gmail.com>
2023-12-12 15:52:38 +0600

    color

--- advanceEvent/src/App.vue
+++ advanceEvent/src/App.vue
@@ -26,30 +26,30 @@ function buttomClick() {
 </script>
 
 <template>
-    <section class="container mx-auto flex items-center flex-col font-bold bg-green-500">
-        <h1 class="text-center text-2xl py-10">Events & Reactiviy</h1>
+    <section class="container mx-auto flex items-center flex-col font-bold bg-sky-200">
+        <h1 class="text-center text-2xl py-10">Advance Events & Reactiviy</h1>
         <h2>{{ message }}</h2>
         <form @submit.prevent action="https://www.google.com/search">
             <div class="container mx-auto flex space-x-5 justify-center m-5">
-                <input @keyup.space="spacePressed()" type="text" name="q" class="border border-gray-500 bg-white p-5"
+                <input @keyup.space="spacePressed()" type="text" name="q" class="border border-gray-500 bg-white p-5 rounded"
                     placeholder="Pas Space">
             </div>
             <div class="container mx-auto flex space-x-5 justify-center m-5">
-                <input @keyup="pressedWhat($event)" type="text" name="q" class="border border-gray-500 bg-white p-5"
+                <input @keyup="pressedWhat($event)" type="text" name="q" class="border border-gray-500 bg-white p-5 rounded"
                     placeholder="Type Somthing">
             </div>
-            <button @click.prevent="buttomPress()" class="bg-blue-500 hover:bg-blue-500 text-white py-2 px-4 rounded mb-5">
+            <button @click.prevent="buttomPress()" class="bg-orange-700 hover:bg-blue-900 text-white py-2 px-4 rounded mb-5">
                 Buttom
             </button>
             <button @click.ctrl.right="ctrlRightPress()"
-                class="bg-blue-500 ml-5 hover:bg-blue-500 text-white py-2 px-4 rounded mb-5">
+                class="bg-blue-500 ml-5 hover:bg-yellow-700 text-white py-2 px-4 rounded mb-5">
                 Mouse Ctrl +Right Click
             </button>
         </form>
 
         <div class="bg-gray-200 w-[300px] h-80 flex items-center justify-center mb-10" @click="divClick()">
             <button @click.stop="buttomClick()"
-                class="bg-orange-500 ml-5 hover:bg-orange-500 text-white py-2 px-4 rounded ">
+                class="bg-black ml-5 hover:bg-orange-500 text-white py-2 px-4 rounded ">
                 Buttom
             </button>
 
