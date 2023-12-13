b4b74ee05dbe2015540b826af072c7b0deff0917
nasir <nasir.joypurhat@gmail.com>
2023-12-13 11:11:08 +0600

    data binding

--- dataBinding/src/App.vue
+++ dataBinding/src/App.vue
@@ -1,11 +1,32 @@
 <script setup>
+import { guardReactiveProps, ref } from 'vue';
+
+const color = ref('');
+
+function getStyle() {
+  return {
+    backgroundColor: color.value,
+  }
+}
 
 </script>
 
 <template>
-
+  <section class="container mx-auto ">
+    <h1 class="text-2xl mb-10">Vue Color Form</h1>
+    <img src="" alt="">
+    <p> Your Favorite Color Name Is: {{ color }}</p>
+    <p>
+      Type Your Favorite Color Name:
+      <input
+        class="p-5 mt-5 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        v-model="color">
+    <div class="w-32 h-32 mt-10 ml-56" :style="getStyle()"></div>
+    </p>
+    <div class="mt-10">
+      <a target="_blank" href="https://nasir369.netlify.app/">Nasir369</a>
+    </div>
+  </section>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ dataBindingObject/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { reactive, ref } from 'vue';
+
+const person = reactive({
+  name:'Jone Doe',
+  age:27,
+  job:'Web Developer'
+});
+
+
+</script>
+
+<template>
+  <section class="container mx-auto ">
+    <h1 class="text-2xl mb-10">Vue Form</h1>
+    <img src="" alt="">
+    <p class="mb-10">  {{ person }}</p>
+    <div class="flex flex-col mb-5 text-left" v-for="(value,key,index) in person" :key="key">
+      <label for="name">{{ key }}</label>
+      <input
+        class="p-5 mt-5 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        v-model="person[key]">
+    </div>
+      
+    
+   
+ 
+  </section>
+</template>
+
+<style scoped></style>
