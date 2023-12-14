4aa3272d403974870809562009a5a7aa1c093d5a
nasir <nasir.joypurhat@gmail.com>
2023-12-14 10:31:32 +0600

    vue life cycle

---
+++ clifeCycle/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+<section class="container mt-5 bg-gray-400 mx-auto">
+<h1 class="text-2xl text-center">Vue Life Cycle</h1>
+</section>
+</template>
+
+<style scoped>
+</style>
