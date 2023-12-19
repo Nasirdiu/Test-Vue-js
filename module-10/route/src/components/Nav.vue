f7a1da9c1bc7f42a5e195be04e20beadfde132c6
nasir <nasir.joypurhat@gmail.com>
2023-12-19 09:56:32 +0600

    design load

---
+++ module-10/route/src/components/Nav.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+    <ul class="items-stretch hidden space-x-3 lg:flex">
+        <li class="flex">
+            <a rel="noopener noreferrer" href="#"
+                class="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Home</a>
+        </li>
+        <li class="flex">
+            <a rel="noopener noreferrer" href="#"
+                class="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400 dark:border-violet-400">Blog</a>
+        </li>
+        <li class="flex">
+            <a rel="noopener noreferrer" href="#"
+                class="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">About</a>
+        </li>
+        <li class="flex">
+            <a rel="noopener noreferrer" href="#"
+                class="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Contact Us</a>
+        </li>
+    </ul>
+</template>
+<style scoped></style>
