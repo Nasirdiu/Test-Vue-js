11750ed9a4b83cf9e5bdb271957cc40fc6044a3f
nasir <nasir.joypurhat@gmail.com>
2023-12-20 10:06:58 +0600

    logout

--- module-11/vue-simple-auth-master/src/data/authstate.js
+++ module-11/vue-simple-auth-master/src/data/authstate.js
@@ -1,14 +1,34 @@
 import { reactive } from "vue";
 import axios from 'axios';
-
+import router from "../router";
 
 const AuthState = reactive({
     isAuthenticated: false,
     user: null,
     username: null,
     password: null,
-    login(){
-        console.log({username:this.username,password:this.password});
+    login() {
+        // console.log({username:this.username,password:this.password});
+
+        axios.post('http://localhost:8001/index.php', {
+            email: this.username,
+            password: this.password
+        }).then((response) => {
+            if (response.data.success == 1) {
+                this.isAuthenticated = true
+                this.user = response.data
+                this.password = null
+                this.username = null
+                router.push('/')
+            }
+
+        })
+
+    },
+    logout(){
+        this.isAuthenticated = false
+        this.user = null
+        router.push('/')
     }
 });
 
