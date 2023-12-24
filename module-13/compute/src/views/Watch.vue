542a3e75c017a888ba56753e8141ab4adf334f45
nasir <nasir.joypurhat@gmail.com>
2023-12-24 17:07:35 +0600

    design

--- module-13/compute/src/views/Watch.vue
+++ module-13/compute/src/views/Watch.vue
@@ -3,17 +3,22 @@
 </script>
  
 <template>
-   <div>
-   <h1>Compute </h1>
-   <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required> <br>
+    <div>
+        <h1 class="text-center text-2xl font-bold">Watch Example</h1>
 
-   <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required>
-   <p class="mt-5">Result :</p>
+        <p class="mt-2 font-bold">Post Id :</p>
+        <p class="mt-2 font-bold">Post Title :</p>
+        <p class="mt-2 font-bold">Post Body :</p>
 
-   <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-5">
-  Button
-</button>
-   </div>
+        <button
+            class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-5">
+            Previus Post
+        </button>
+        <button
+            class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-5 ml-3">
+            Next Post
+        </button>
+    </div>
 </template>
  
 <style scoped></style>
