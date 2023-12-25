f06a0d40b91320cf7fddbf700c1bfac596939157
Md.Nasir Uddin <nasir.joypurhat@gmail.com>
2023-12-25 12:07:09 +0600

    watch

--- module-13/compute/src/views/Watch.vue
+++ module-13/compute/src/views/Watch.vue
@@ -1,24 +1,65 @@
 <script setup>
+import { onBeforeMount, ref, watch, watchEffect } from "vue";
 
+const postId = ref(1);
+const post = ref({});
+
+const fetchPost = () => {
+  fetch(`https://jsonplaceholder.typicode.com/posts/${postId.value}`)
+    .then((response) => response.json())
+    .then((json) => (post.value = json));
+};
+// onBeforeMount(() => {
+//   fetchPost();
+// });
+
+// ata diye kaj hba
+
+// watch(
+//   postId,
+//   () => {
+//     fetchPost();
+//   },
+//   { immediate: true }
+// );
+
+// or
+
+watchEffect(() => {
+  fetchPost();
+});
+
+const previousPost = () => {
+  postId.value <= 1 ? (postId.value = 1) : postId.value--;
+  // fetchPost()
+};
+const nextPost = () => {
+  postId.value++;
+  // fetchPost()
+};
 </script>
- 
+
 <template>
-    <div>
-        <h1 class="text-center text-2xl font-bold">Watch Example</h1>
+  <div>
+    <h1 class="text-center text-2xl font-bold">Watch Example</h1>
 
-        <p class="mt-2 font-bold">Post Id :</p>
-        <p class="mt-2 font-bold">Post Title :</p>
-        <p class="mt-2 font-bold">Post Body :</p>
+    <p class="mt-2 font-bold"><strong>Post Id :</strong>{{ postId }}</p>
+    <p class="mt-2 font-bold"><strong>Post Title :</strong>{{ post.title }}</p>
+    <p class="mt-2 font-bold"><strong>Post Body :</strong>{{ post.body }}</p>
 
-        <button
-            class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-5">
-            Previus Post
-        </button>
-        <button
-            class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-5 ml-3">
-            Next Post
-        </button>
-    </div>
+    <button
+      @click="previousPost()"
+      class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-5"
+    >
+      Previus Post
+    </button>
+    <button
+      @click="nextPost()"
+      class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-5 ml-3"
+    >
+      Next Post
+    </button>
+  </div>
 </template>
- 
+
 <style scoped></style>
