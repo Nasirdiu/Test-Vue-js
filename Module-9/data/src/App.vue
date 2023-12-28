4fae77cc377ac8fcda756590b05deeee07d6d27e
nasir <nasir.joypurhat@gmail.com>
2023-12-28 13:02:00 +0600

    data load

--- Module-9/data/src/App.vue
+++ Module-9/data/src/App.vue
@@ -1,10 +1,68 @@
 <script setup>
 
+import axios from 'axios';
+import { ref } from 'vue';
+
+const tableData = ref([]);
+
+list();
+async function list() {
+    let url = "https://crud.teamrabbil.com/api/v1/ReadProduct"
+    let res = await axios.get(url)
+    let data = res.data['data']
+    tableData.value = data;
+}
 </script>
 
 <template>
-  
-
-<style scoped>
-
-</style>
+    <div class="flex flex-col">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="overflow-hidden">
+                    <table class="min-w-full">
+                        <thead class="bg-white border-b">
+                            <tr>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                    #
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                    First
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                    Last
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                    Handle
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for=" item in tableData" class="bg-gray-100 border-b">
+                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
+                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    {{ item['_id'] }}
+                                </td>
+                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    {{ item['ProductName'] }}
+                                </td>
+                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    {{ item['ProductCode'] }}
+                                </td>
+                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    {{ item['UnitPrice'] }}
+                                </td>
+                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    {{ item['Qty'] }}
+                                </td>
+                                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    {{ item['TotalPrice'] }}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped></style>
