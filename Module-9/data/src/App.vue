800b43e6e1bcab011ff69a35e262aec1ba45d5f6
nasir <nasir.joypurhat@gmail.com>
2023-12-31 12:22:30 +0600

    design

--- Module-9/data/src/App.vue
+++ Module-9/data/src/App.vue
@@ -46,6 +46,9 @@ async function list() {
                                 <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                                     CreatedDate
                                 </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                    Action
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
@@ -73,6 +76,12 @@ async function list() {
                                 <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                     {{ item['CreatedDate'] }}
                                 </td>
+                                <button type="button"
+                                    class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Edit</button>
+
+                                <button type="button"
+                                    class=" ml-3 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+
                             </tr>
                         </tbody>
                     </table>
